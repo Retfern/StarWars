83843a36010e8c19095aa5b5442832963a619857
Alexey Kutepov <alkutepov@gmail.com>
2019-04-08 20:40:51 +0300

    Урок 2 часть 1

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -6,35 +6,59 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	TextureRegion region;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 100, 100, 150, 150);
-		region.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-	}
+    SpriteBatch batch;
+    Texture background;
+    Texture img;
+    TextureRegion region;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0.25f, 0.15f, 0.37f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.setColor(0.454f, 0.151f, 0.959f, 1f);
-		batch.draw(img, 100, 100);
-		batch.setColor(0.666f, 0.777f, 0.1313f, 1f);
-		batch.draw(region, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("bg.png");
+        img = new Texture("badlogic.jpg");
+
+        Vector2 v1 = new Vector2();
+        Vector2 v2 = new Vector2();
+        v1.set(5,5);
+        v2.set(2, 1);
+        v1.add(v2);
+        System.out.println("add v.x = " + v1.x + " v.y = " + v1.y);
+        v1.set(10, 6);
+        v2.set(3, 11);
+        Vector2 v3;
+        v3 = v1.cpy().sub(v2);
+        System.out.println("sub v.x = " + v1.x + " v.y = " + v1.y);
+        System.out.println("sub v.x = " + v3.x + " v.y = " + v3.y);
+        System.out.println("v1.len() = " + v1.len());
+        System.out.println("v3.len() = " + v3.len());
+        v3.nor();
+        System.out.println("sub v.x = " + v3.x + " v.y = " + v3.y);
+        System.out.println("v3.len() = " + v3.len());
+
+        v1.set(4,9);
+        v2.set(5,13);
+        v1.nor();
+        v2.nor();
+        System.out.println(Math.acos((v1.dot(v2))));
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.25f, 0.15f, 0.37f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(background, 0, 0);
+        batch.draw(img, 100, 100);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        background.dispose();
+        img.dispose();
+    }
 }
