69f85108b229e7c7d8d6f98c3153c9c8f1deb203
Alexey Kutepov <alkutepov@gmail.com>
2019-04-11 20:53:46 +0300

    Урок 3 часть 1

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -8,35 +8,23 @@ import ru.geekbrains.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
-    private static float V_LEN = 0.5f;
 
-    private Vector2 touch;
     private Vector2 pos;
-    private Vector2 v;
-    private Vector2 buf;
     private Texture img;
 
     @Override
     public void show() {
         super.show();
-        touch = new Vector2();
         pos = new Vector2();
-        v = new Vector2(0.2f,0.5f);
-        buf = new Vector2();
         img = new Texture("badlogic.jpg");
+        batch.getProjectionMatrix().idt();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        buf.set(touch);
-        if (buf.sub(pos).len() > V_LEN) {
-            pos.add(v);
-        } else {
-            pos.set(touch);
-        }
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(img, -0.5f, -0.5f, 1f, 1f);
         batch.end();
     }
 
@@ -47,11 +35,7 @@ public class MenuScreen extends BaseScreen {
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
-        System.out.println("touchDown touch.x = " + touch.x + " touch.y = " + touch.y);
+    public boolean touchDown(Vector2 touch, int pointer) {
         return false;
     }
-
 }
